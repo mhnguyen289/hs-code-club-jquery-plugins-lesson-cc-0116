eb29f92a3b2dd6e069a95c306c59ca38a40218a2
mhnguyen289 <mhnguyen289@gmail.com>
2016-04-19 15:16:43 -0700

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
- $('document').ready(function(){
+$('document').ready(function(){
 
 
   //try Fart Scroll here
+  fartscroll();
 
   //try fool.js here
-
+  $.fool('vanishingElements');
+//  $.fool('questionTime');
+//  $.fool('upsideDown');
+//  $.fool('h4xx0r');
+//  $.fool('wonky');
+//  $.fool('flash');
+//  $.fool('crashAndBurn');
+//  $.fool('shutter');
+//  $.fool('unclickable');
 
 });
